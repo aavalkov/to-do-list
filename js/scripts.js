34afc0ece5f852f98da2b5edd3bdfb19c614ec9d
Meghan Lindsley & Anna Valkov <student@epicodus.com>
2014-07-15 11:34:57 -0700

    Working product with To Do List and Completed List.

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,19 @@ $(document).ready(function() {
     var listDate = $("input#date").val();
     var inputtedItemDate = { item: listItem, date: listDate};
 
-    $("ul#list").append("<li>" + inputtedItemDate.item +
-                        " by " + inputtedItemDate.date + "</li>");
+    $("ul#list").append("<li><span class='clickItem'>" + inputtedItemDate.item +
+                        " by " + inputtedItemDate.date + "</span></li>");
 
     $("input#item").val("");
     $("input#date").val("");
+
+    $("ul#list").children("li").last().click(function() {
+      $(".col-md-5").show();
+      $("ul#done").append("<li><span class = 'finishedItem'>" +
+                            inputtedItemDate.item + " " +
+                            inputtedItemDate.date + "</span></li>");
+      $(this).remove();
+
+    });
   });
 });
